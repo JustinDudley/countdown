5d612df1dfcc4d22690cacd43a5e3f59caf3e48e
justindudley <justin.dudley@ascendum.com>
2020-07-15 16:09:56 -0400

    add state, put real data into Flatlist

--- EventList.js
+++ EventList.js
@@ -2,11 +2,26 @@ import React, { Component } from "react";
 import { FlatList, Text } from "react-native";
 
 class EventList extends Component {
+  //default state for this component:
+  state = {
+    events: [],
+  };
+
+  // to get the data that we want to bind to:
+  componentDidMount() {
+    const events = require("./db.json").events;
+    this.setState({ events });
+  }
+
   render() {
     return (
       <FlatList
-        data={[{ name: "a" }, { name: "b" }]}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        // data={[{ name: "a" }, { name: "b" }]}
+        // renderItem={({ item }) => <Text>{item.name}</Text>}
+        // to bind to real data:
+        data={this.state.events}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={(item) => item.id}
       />
     );
   }
