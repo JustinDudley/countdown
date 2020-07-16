b17d53db01c4be267e473c61eea9262c92013a60
justindudley <justin.dudley@ascendum.com>
2020-07-16 12:34:02 -0400

    EventForm, which is now sadly a function component, contains a button that DOES navigate back to EventList

--- App.js
+++ App.js
@@ -1,26 +1,51 @@
 import "react-native-gesture-handler"; // placed at very top, per React Native docs
-import React from "react";
+import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import EventList from "./EventList";
+import { EventForm } from "./EventForm";
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator(); // returns an object containing 2 properties: Screen and Navigator
 
-//The class below is all going to get deleted by Hendrik
-export default class App extends React.Component {
+export default class MyStack extends React.Component {
   render() {
     return (
-      // NavigationContainer is from RN docs, not Hendriks
+      // verbage: "Each SCREEN in the NAVIGATOR can specify some options for the navigator, such as the title to render in the header."
+
       <NavigationContainer>
-        <EventList />
+        <Stack.Navigator initialRouteName="EventForm">
+          <Stack.Screen
+            name="EventList"
+            component={EventList}
+            options={{ title: "this is the EventList" }}
+          />
+          <Stack.Screen
+            name="EventForm"
+            component={EventForm}
+            options={{ title: "this is the EventForm" }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     );
   }
 }
 
+// //The class below is all going to get deleted by Hendrik
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       // NavigationContainer is from RN docs, not Hendriks
+//       <NavigationContainer>
+//         <EventList />
+//       </NavigationContainer>
+//     );
+//   }
+// }
+
 //
-// -- or --
+//
+// -- function component, works --
 // export default function App() {
 //   debugger;
 //   return <EventList />;
--- EventForm.js
+++ EventForm.js
@@ -1,21 +1,52 @@
 import React, { Component } from "react";
-import { View, Text, TouchableHighlight, Alert } from "react-native";
+import { View, Text, TouchableHighlight, Alert, Button } from "react-native";
 
-class EventForm extends Component {
-  handleAddPress = () => {
+export function EventForm({ navigation }) {
+  const handleAddPress = () => {
     Alert.alert("you pressed add");
+    navigation.navigate("EventList");
+
+    // this.navigation.navigate("EventList");
+
     // ?   (navigation logic will go here)
   };
 
-  render() {
-    return (
-      <View>
-        <TouchableHighlight onPress={this.handleAddPress}>
-          <Text>Add</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
+  return (
+    <View>
+      <Button
+        title="Add this way"
+        onPress={() => navigation.navigate("EventList")}
+      ></Button>
+      <TouchableHighlight onPress={() => handleAddPress}>
+        <Text>Add</Text>
+      </TouchableHighlight>
+    </View>
+  );
 }
 
-export default EventForm;
+// class EventForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     // this.state = navigation;
+//   }
+
+//   handleAddPress = () => {
+//     Alert.alert("you pressed add");
+
+//     // this.navigation.navigate("EventList");
+
+//     // ?   (navigation logic will go here)
+//   };
+
+//   render() {
+//     return (
+//       <View>
+//         <TouchableHighlight onPress={this.handleAddPress}>
+//           <Text>Add</Text>
+//         </TouchableHighlight>
+//       </View>
+//     );
+//   }
+// }
+
+// export default EventForm;
