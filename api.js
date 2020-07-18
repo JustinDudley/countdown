8452f172a00b452c972e22eceac0a44b69413245
justindudley <justin.dudley@ascendum.com>
2020-07-17 23:21:20 -0400

    use ngrok for getEvent's url

--- api.js
+++ api.js
@@ -1,39 +1,17 @@
 import moment from "moment";
 
-// const url = "http://localhost:3000/events";
-// this won't work because i'm using my phone, not my laptop,  so we do VOO-DOO:
+// TO GENERATE NGROK, use 2 (TWO) terminals:
+// >countdown $ json-server --watch db.json
+// >countdown $ ngrok http 3000
 
-import Constants from "expo-constants";
-const { manifest } = Constants;
-
-const api =
-  typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
-    ? /*console.log("so far so good") */
-      manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
-    : `api.example.com`;
-
-// replaced below with stack overflow
-// import Constants from "expo-constants";
-// const { manifest } = Constants;
-// const api = manifest.packagerOpts.dev
-//   ? manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
-//   : `api.example.com`;
-
-const url = `http://${api}/events`;
-console.log("url is: ", url);
+const ngrokWithHttp = "http://f77fb5466f91.ngrok.io";
+const url = `${ngrokWithHttp}/events`;
 
 export function getEvents() {
   return fetch(url)
     .then((response) => response.json())
-    .then((events) => console.log(events));
-  // .then((events) => events.map((e) => ({ ...e, date: new Date(e.date) })));
+    .then((events) => events.map((e) => ({ ...e, date: new Date(e.date) })));
 }
-
-// export const getEvents = () => {
-//   return fetch(url)
-//     .then((res) => res.json())
-//     .then((events) => events.map((e) => ({ ...e, date: new Date(e.date) })));
-// };
 
 export function formatDateTime(dateString) {
   const parsed = moment(new Date(dateString));
@@ -66,3 +44,15 @@ export function getCountdownParts(eventDate) {
     seconds: duration.get("seconds"),
   };
 }
+
+//
+// const url = "http://localhost:3000/events";
+// this won't work because i'm using my phone, not my laptop,  so we do VOO-DOO:
+
+// Couldn't make this Hendrik plan (for generating url) work, so used ngrok instead:
+// import Constants from "expo-constants";
+// const { manifest } = Constants;
+// const api =
+//   typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
+//     ? manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
+//     : `api.example.com`;
